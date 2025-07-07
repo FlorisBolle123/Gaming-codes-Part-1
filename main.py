3c94852f2e0c93918b60334880e992f550dede89
FlorisBolle123 <flozzy2018fortnite@gmail.com>
2025-07-07 20:48:11 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,78 @@
 import pgzrun
-from random import randint
-from time import time
+import random
 
+#screen size
 WIDTH=800
 HEIGHT=600
 
-cheeses=[]
+CENTRE=(400,300)
+FINAL_LEVEL = 6
+START_SPEED = 10
+ITEMS = ["battery","bottle","chipbag","paperbag","plasticbag"]
 
-start_time=0
-total_time=0
-end_time=0
+game_over = False
+game_complete = False
+current_level = 1
 
-number_of_cheese=10
+items=[]
+animations=[]
 
-def create_cheese():
-    global start_time
-    for count in range(0,number_of_cheese):
-        cheese=Actor("cheese")
-        cheese.pos=randint(40,WIDTH-40), randint(40,HEIGHT-40)
-        cheeses.append(cheese)
-    start_time=time()
-
-def on_mouse_down(pos):
-    #global mouse
-    for i in cheeses:
-        #if cheeses[i].collidepoint(pos):
-            #mouse=Actor("rat_mouse")
-
-
+#anounce the draw function
 def draw():
-    global total_time
-    screen.blit("bg",(0,0))
-    number=1
-    for cheese in cheeses:
-        screen.draw.text(str(number),(cheese.pos[0],cheese.pos[1]+20))
-        cheese.draw()
-        number=number+1
+    global items, current_level, game_over, game_complete
+    screen.clear()
+    screen.blit("background", (0,0))
+    if game_over:
+        display_message("Game over","Try again...")
+    elif game_complete:
+        display_message("You won","Well done...")
+    else: 
+        for item in items:
+            item.draw()
 
-    
-    
+#global items
+def update():
+    global items
+    if len(items) == 0:
+        items = make_items(current_level)
 
-create_cheese()
-pgzrun.go()
+# this function helps you animate the items and the layout
+def make_items(number_of_extra_items):
+    items_to_create=get_option_to_create(number_of_extra_items)
+    new_items=create_items(items_to_create)
+    layout_items(new_items)
+    animate_items(new_items)
+    return new_items
+
+# this helps you randomly choose items
+def get_option_to_create(number_of_extra_items):
+    items_to_create=["paperbag"]
+    for i in range(0,number_of_extra_items):
+        random_option=random.choice(ITEMS)
+        items_to_create.append(random_option)
+    return items_to_create
+
+# This is the function that draws the image/item on the screen
+def create_items(items_to_create):
+    new_items=[]
+    for option in items_to_create:
+        item=Actor(option+"img")
+        new_items.append(item)
+    return new_items
+
+#this makes the layout of where everything is
+def layout_items(items_to_layout):
+    number_of_gaps=len(items_to_layout)+1
+    gap_size=WIDTH / number_of_gaps
+    random.shuffle(items_to_layout)
+    for index, item in enumerate(items_to_layout):
+        new_x_pos=(index+1)*gap_size
+        item.x=new_x_pos
+#This makes the animations for the items
+def animate_items(items_to_animate):
+    global animations
+    for item in items_to_animate:
+        duration = START_SPEED - current_level
+        item.anchor=("center","bottom")
+        animation = animate(item,duration=duration,on_finished=handle_game_over,y=HEIGHT)
+        animations.append(animation)
